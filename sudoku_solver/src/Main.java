e01c859d598a01547f7393c3ed76b7b08df8f7b4
Evgenij Bogdanov <gekabog@gmail.com>
2019-03-31 23:16:43 +0300

    added args input

--- sudoku_solver/src/Main.java
+++ sudoku_solver/src/Main.java
@@ -3,6 +3,6 @@ public class Main {
     public static void main(String[] args) {
 
         SudokuSolver sudokuSolver = new SudokuSolver();
-        sudokuSolver.solve("res/input.txt");
+        sudokuSolver.solve(args[0]);
     }
 }
